84d6aad2fbe69b1d73cdf66a9505db38e3abf4e1
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-04-13 16:26:49 -0500

    Added ability to save scores on screen orientation change or app exit to reload when activity is restarted

--- GolfScoreboard/app/src/main/java/com/example/golfscoreboard/MainActivity.java
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.golfscoreboard;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +12,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_FILE = "com.example.golfscoreboard.preferences";
+    private static final String KEY_SCORE1 = "key_score1";
+    private static final String KEY_SCORE2 = "key_score2";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
     final Scoreboard scoreboard = new Scoreboard();
 
     Button hole1Plus;
@@ -32,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
         hole1Score = findViewById(R.id.hole1Score);
         hole2Score = findViewById(R.id.hole2Score);
+
+        sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        loadScores();
+
 
         hole1Plus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +72,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void loadScores() {
+        String loadScore1 = sharedPreferences.getString(KEY_SCORE1, "");
+        String loadScore2 = sharedPreferences.getString(KEY_SCORE2, "");
+        hole1Score.setText(loadScore1);
+        hole2Score.setText(loadScore2);
+
+        scoreboard.setHoleScore(1, loadScore1);
+        scoreboard.setHoleScore(2, loadScore2);
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
@@ -79,20 +102,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        editor.putString(KEY_SCORE1, scoreboard.getHoleScore(1));
+        editor.putString(KEY_SCORE2, scoreboard.getHoleScore(2));
+
+        editor.apply();
+    }
+
     private String increaseHoleScore(int hole) {
         scoreboard.increaseScore(hole);
-        return scoreboard.getHoleScore(hole) + "";
+        return scoreboard.getHoleScore(hole);
     }
     private String decreaseHoleScore(int hole) {
         scoreboard.decreaseScore(hole);
-        return scoreboard.getHoleScore(hole) + "";
+        return scoreboard.getHoleScore(hole);
     }
 
     private void resetAllScores() {
         hole1Score.setText("0");
         hole2Score.setText("0");
     }
-
-    //TODO: Remember the scores we set (even if we close the app)
 
 }
--- GolfScoreboard/app/src/main/java/com/example/golfscoreboard/Scoreboard.java
+++ GolfScoreboard/app/src/main/java/com/example/golfscoreboard/Scoreboard.java
@@ -3,8 +3,12 @@ package com.example.golfscoreboard;
 public class Scoreboard {
     private int[] scoreboard = new int[18];
 
-    public int getHoleScore(int hole) {
-        return scoreboard[hole - 1];
+    public String getHoleScore(int hole) {
+        return scoreboard[hole - 1] + "";
+    }
+
+    public void setHoleScore(int hole, String score) {
+        this.scoreboard[hole - 1] = Integer.parseInt(score);
     }
 
     public void decreaseScore(int hole) {
